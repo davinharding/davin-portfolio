eec510d1d8234d8eef53812e73d78a0be70e3475
davinharding <davinlharding@gmail.com>
2023-10-08 17:40:41 -0400

    added profile pic, added headless ui and working on adding better dropdown menu component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
 // components/Header.tsx
 
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faHouse } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
-import { useState } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import { Fragment, useEffect, useRef, useState } from "react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
 
 const Header: React.FC = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -14,7 +16,7 @@ const Header: React.FC = () => {
         <Link href={"/"}>DH</Link>
 
         {/* Desktop Links */}
-        <ul className="hidden md:flex space-x-4">
+        <ul className="hidden md:flex space-x-4 bg-gray-800">
           <li>
             <Link href="/about">
               <button className="text-white hover:text-gray-400">About</button>
@@ -42,39 +44,156 @@ const Header: React.FC = () => {
 
       {/* Mobile Sidebar Menu */}
       {isOpen && (
-        <div
-          className="absolute top-10 right-0 transform translate-x-0 md:translate-x-full transition-transform duration-300 bg-gray-800 h-full w-1/4 z-40"
-          style={{ maxWidth: "25%" }}
-        >
-          <ul className="flex flex-col space-y-2 p-4">
-            <li>
-              <Link href="/about">
+        <>
+          <div className="fixed top-16 w-56 text-right">
+            <Menu as="div" className="relative inline-block text-left">
+              <div>
+                <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+                  Options
+                  <FontAwesomeIcon icon={faHouse} />
+                </Menu.Button>
+              </div>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                  <div className="px-1 py-1 ">
+                    <Menu.Item>
+                      {({ active }) => (
+                        <button
+                          className={`${
+                            active
+                              ? "bg-violet-500 text-white"
+                              : "text-gray-900"
+                          } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                        >
+                          {active ? (
+                            <FontAwesomeIcon icon={faHouse} />
+                          ) : (
+                            <FontAwesomeIcon icon={faHouse} />
+                          )}
+                          Edit
+                        </button>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <button
+                          className={`${
+                            active
+                              ? "bg-violet-500 text-white"
+                              : "text-gray-900"
+                          } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                        >
+                          {active ? (
+                            <FontAwesomeIcon icon={faHouse} />
+                          ) : (
+                            <FontAwesomeIcon icon={faHouse} />
+                          )}
+                          Duplicate
+                        </button>
+                      )}
+                    </Menu.Item>
+                  </div>
+                  <div className="px-1 py-1">
+                    <Menu.Item>
+                      {({ active }) => (
+                        <button
+                          className={`${
+                            active
+                              ? "bg-violet-500 text-white"
+                              : "text-gray-900"
+                          } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                        >
+                          {active ? (
+                            <FontAwesomeIcon icon={faHouse} />
+                          ) : (
+                            <FontAwesomeIcon icon={faHouse} />
+                          )}
+                          Archive
+                        </button>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <button
+                          className={`${
+                            active
+                              ? "bg-violet-500 text-white"
+                              : "text-gray-900"
+                          } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                        >
+                          {active ? (
+                            <FontAwesomeIcon icon={faHouse} />
+                          ) : (
+                            <FontAwesomeIcon icon={faHouse} />
+                          )}
+                          Move
+                        </button>
+                      )}
+                    </Menu.Item>
+                  </div>
+                  <div className="px-1 py-1">
+                    <Menu.Item>
+                      {({ active }) => (
+                        <button
+                          className={`${
+                            active
+                              ? "bg-violet-500 text-white"
+                              : "text-gray-900"
+                          } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                        >
+                          {active ? (
+                            <FontAwesomeIcon icon={faHouse} />
+                          ) : (
+                            <FontAwesomeIcon icon={faHouse} />
+                          )}
+                          Delete
+                        </button>
+                      )}
+                    </Menu.Item>
+                  </div>
+                </Menu.Items>
+              </Transition>
+            </Menu>
+          </div>
+          {/* <div className="fixed top-10 right-0 h-full w-4/5 max-w-md transform ease-in-out atranslate-x-0 md:translate-x-full transition-transform duration-600 bg-gray-800 z-40">
+            <ul className="flex flex-col space-y-2 p-4 bg-gray-800">
+              <li>
+                <Link href="/about">
+                  <button
+                    className="text-white hover:text-gray-400 w-full text-left"
+                    onClick={() => setIsOpen(false)}
+                  >
+                    About
+                  </button>
+                </Link>
+              </li>
+              <li>
                 <button
                   className="text-white hover:text-gray-400 w-full text-left"
                   onClick={() => setIsOpen(false)}
                 >
-                  About
+                  Portfolio
                 </button>
-              </Link>
-            </li>
-            <li>
-              <button
-                className="text-white hover:text-gray-400 w-full text-left"
-                onClick={() => setIsOpen(false)}
-              >
-                Portfolio
-              </button>
-            </li>
-            <li>
-              <button
-                className="text-white hover:text-gray-400 w-full text-left"
-                onClick={() => setIsOpen(false)}
-              >
-                Book a Call
-              </button>
-            </li>
-          </ul>
-        </div>
+              </li>
+              <li>
+                <button
+                  className="text-white hover:text-gray-400 w-full text-left"
+                  onClick={() => setIsOpen(false)}
+                >
+                  Book a Call
+                </button>
+              </li>
+            </ul>
+          </div> */}
+        </>
       )}
     </div>
   );
--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,4 +1,3 @@
-// components/MainContent.tsx
 import { useState, useEffect } from "react";
 import Background from "./Background";
 
@@ -9,6 +8,7 @@ const MainContent: React.FC = () => {
     "Mint Pages",
     "Smart Contracts",
     "Landing Pages",
+    "SaaS Platforms"
   ];
   const [currentWord, setCurrentWord] = useState<number>(0);
 
@@ -23,20 +23,28 @@ const MainContent: React.FC = () => {
   return (
     <Background>
       <div className="flex items-center justify-center h-screen">
-        <div className="lg:w-5/12 w-9/12">
-        <h1 className="lg:text-6xl text-2xl font-bold">Hi, {"I'm"} Davin.</h1>
-          <h1 className="lg:text-6xl text-2xl font-bold">
-            I build{" "}
-            <span className={`inline-block`}>
-              <span
-                key={currentWord}
-                className="block bg-clip-text text-transparent bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 animate-slide pb-4"
-              >
-                {words[currentWord]}
+        <div className="lg:w-1/2 w-9/12 flex justify-between items-center">
+          <div>
+            <h1 className="lg:text-6xl text-2xl font-bold">Hi, {"I'm"} Davin.</h1>
+            <h1 className="lg:text-6xl text-2xl font-bold">
+              I build{" "}
+              <span className={`inline-block`}>
+                <span
+                  key={currentWord}
+                  className="block bg-clip-text text-transparent bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 animate-slide pb-4"
+                >
+                  {words[currentWord]}
+                </span>
               </span>
-            </span>
-            .
-          </h1>
+              .
+            </h1>
+          </div>
+
+          <img
+            src="/profile_picture.jpg"
+            alt="Davin's Profile"
+            className="rounded-full h-28 lg:h-72 shadow-lg shadow-inherit transform transition-transform duration-300 ml-6"
+          />
         </div>
 
         <style jsx>{`
