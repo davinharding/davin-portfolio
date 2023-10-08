c4b4ef5901ff79321801333ba89be2c27a389f15
davinharding <davinlharding@gmail.com>
2023-10-08 18:21:27 -0400

    Fade test

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,7 +1,7 @@
 // components/Timeline.tsx
 import React from "react";
 import { Chrono } from "react-chrono";
-import { Fade } from 'react-awesome-reveal';
+import { Fade } from "react-awesome-reveal";
 
 const items = [
   {
@@ -9,73 +9,77 @@ const items = [
     cardTitle: "Card Title 1",
     cardSubtitle: "Card Subtitle 1",
     cardDetailedText: "Detailed description about Event 1.",
-    id: 1
+    id: 1,
   },
   {
     title: "Event 2",
     cardTitle: "Card Title 2",
     cardSubtitle: "Card Subtitle 2",
     cardDetailedText: "Detailed description about Event 2.",
-    id: 2
+    id: 2,
   },
   {
     title: "Event 1",
     cardTitle: "Card Title 1",
     cardSubtitle: "Card Subtitle 1",
     cardDetailedText: "Detailed description about Event 1.",
-    id: 3
+    id: 3,
   },
   {
     title: "Event 2",
     cardTitle: "Card Title 2",
     cardSubtitle: "Card Subtitle 2",
     cardDetailedText: "Detailed description about Event 2.",
-    id: 4
+    id: 4,
   },
   {
     title: "Event 1",
     cardTitle: "Card Title 1",
     cardSubtitle: "Card Subtitle 1",
     cardDetailedText: "Detailed description about Event 1.",
-    id: 5
+    id: 5,
   },
   {
     title: "Event 2",
     cardTitle: "Card Title 2",
     cardSubtitle: "Card Subtitle 2",
     cardDetailedText: "Detailed description about Event 2.",
-    id: 6
+    id: 6,
   },
   {
     title: "Event 1",
     cardTitle: "Card Title 1",
     cardSubtitle: "Card Subtitle 1",
     cardDetailedText: "Detailed description about Event 1.",
-    id: 7
+    id: 7,
   },
   {
     title: "Event 2",
     cardTitle: "Card Title 2",
     cardSubtitle: "Card Subtitle 2",
     cardDetailedText: "Detailed description about Event 2.",
-    id: 8
+    id: 8,
   },
   // Add more events as required
 ];
 
 const Timeline: React.FC = () => {
   return (
-    <div style={{ width: '100%', height: '100%', backgroundColor: 'rgba(37, 39, 89, 0.8)' }}>
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        backgroundColor: "rgba(37, 39, 89, 0.8)",
+      }}
+    >
       <Fade cascade>
-        <div className="text-6xl">test</div>
-      </Fade>
         <Chrono
           theme={{
-            primary: '#ffffff',
-            secondary: '#7C3AED',
-            cardBgColor: '#373B59',
-            cardForeColor: '#ffffff',
-            titleColor: '#D1D5DB',
+            primary: "#ffffff",
+            secondary: "#7C3AED",
+            cardBgColor: "#373B59",
+            cardForeColor: "#ffffff",
+            titleColor: "#D1D5DB",
           }}
           items={items}
           mode="VERTICAL_ALTERNATING"
@@ -87,7 +91,7 @@ const Timeline: React.FC = () => {
           scrollable={{ scrollbar: false }}
           hideControls
         />
-      
+      </Fade>
     </div>
   );
 };
