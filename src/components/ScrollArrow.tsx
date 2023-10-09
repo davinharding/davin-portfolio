262f8e32f5f817ac288021a16535a577830be5d2
davinharding <davinlharding@gmail.com>
2023-10-09 19:10:24 -0400

    added timeline id to scroll arrow

--- src/components/ScrollArrow.tsx
+++ src/components/ScrollArrow.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const ScrollArrow: React.FC = () => {
   return (
-    <div className="absolute bottom-5 left-0 w-full flex justify-center">
+    <div className="absolute bottom-5 left-0 w-full flex justify-center" id="timeline">
       <div className="animate-bounce text-3xl">
         <Link href="#timeline" className="scroll-smooth">
           <FontAwesomeIcon icon={faChevronDown} />
