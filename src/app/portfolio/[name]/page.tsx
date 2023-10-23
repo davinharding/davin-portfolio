b98b3ba68633c0ee3334ce8cf81cda6f8cd1ab6b
davinharding <davinlharding@gmail.com>
2023-10-23 17:15:34 -0400

    added templating to project detail page

--- src/app/portfolio/[name]/page.tsx
+++ src/app/portfolio/[name]/page.tsx
@@ -1,10 +1,74 @@
-'use client'
+"use client";
 
+import Image from "next/image";
 import ContainerLayout from "@/components/ContainerLayout";
+import { projects, Project } from "@/data/projectData";
 
-const PortfolioProjectDetail = ({ params }: { params: { name: string }}) => (
-  <ContainerLayout pageTitle={decodeURI(params.name)}>
-  </ContainerLayout>
-);
+const PortfolioProjectDetail = ({ params }: { params: { name: string } }) => {
+  // Filter the project based on the name parameter.
+  const project: Project | undefined = projects.find(
+    (p) => p.name === decodeURI(params.name),
+  );
+
+  if (!project) {
+    return (
+      <ContainerLayout pageTitle="Project Not Found">
+        Project Not Found
+      </ContainerLayout>
+    );
+  }
+
+  return (
+    <ContainerLayout pageTitle={project.name} mobile>
+      <div className="lg:max-w-screen-xl">
+        <div className="flex-1 pr-6 my-6">
+          <div>{project.description}</div>
+        </div>
+
+        <div className="flex flex-wrap flex-1">
+          {project.image && (
+            <Image
+              src={project.image}
+              alt={project.title}
+              className="w-full mb-4 object-cover"
+            />
+          )}
+          <div className="w-1/2 pr-2">
+            {project.image2 && (
+              <Image
+                src={project.image2}
+                alt={`${project.title} - 2`}
+                className="w-full mb-4 object-cover"
+              />
+            )}
+          </div>
+          <div className="w-1/2 pl-2">
+            {project.image3 && (
+              <Image
+                src={project.image3}
+                alt={`${project.title} - 3`}
+                className="w-full mb-4 object-cover"
+              />
+            )}
+          </div>
+          {project.image4 && (
+            <Image
+              src={project.image4}
+              alt={`${project.title} - 4`}
+              className="w-1/2 pr-2 mb-4 object-cover"
+            />
+          )}
+          {project.image5 && (
+            <Image
+              src={project.image5}
+              alt={`${project.title} - 5`}
+              className="w-1/2 pl-2 mb-4 object-cover"
+            />
+          )}
+        </div>
+      </div>
+    </ContainerLayout>
+  );
+};
 
 export default PortfolioProjectDetail;
